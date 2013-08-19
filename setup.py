c9ed452b3b6799f7d54b079e44a8ca05d27a98c9
Stephane Robino <stephane.robino@gmail.com>
2013-08-19 09:43:48 +0200

    add folders to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Fortylines LLC',
     author_email='support@fortylines.com',
-    packages=['saas'],
+    packages=['saas','saas.views','saas.static','saas.backends','saas.management','saas.management','saas.urls','saas.templatetags','saas.templates','saas.migrations'],
     url='https://github.com/fortylines/saas_framework/',
     license='LICENSE',
     description='Fortylines SaaS implementation',
