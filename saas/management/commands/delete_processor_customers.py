461da59cec9d9d0d97d150a0678a1ed56c8964e0
Sebastien Mirolo <smirolo@fortylines.com>
2013-08-29 10:53:51 -0700

    new command to delete customer on the processor service and improve name of import log command

---
+++ saas/management/commands/delete_processor_customers.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2013, Fortylines LLC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from saas.backends import list_customers
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('-n',
+            action='store_true',
+            dest='no_execute',
+            default=False,
+            help='Print but do not execute'),
+        )
+    help = """Delete the (customer) account associated with an organization
+from the payment processor service."""
+    args = 'regex'
+
+    def handle(self, *args, **options):
+        pat = r'.*'
+        if len(args) > 0:
+            pat = args[0]
+        for cust in list_customers(pat):
+            sys.stdout.write('%s %s\n' % (str(cust.id), str(cust.description)))
+            if not options['no_execute']:
+                cust.delete()
+
+
+
