8e8d0274776474d6ba5c7b99eedd58b9e54140a1
Sebastien Mirolo <smirolo@fortylines.com>
2013-10-17 11:54:32 -0700

    add subscriptions to south migration

--- testsite/settings.py
+++ testsite/settings.py
@@ -168,6 +168,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django.contrib.markup',
+    'south',
     'app_metrics',
     'saas',
     'testsite'
