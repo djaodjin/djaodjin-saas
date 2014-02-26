a9c0b4db4c15fdea5528487eed4b775991842bf7
Sebastien Mirolo <smirolo@fortylines.com>
2014-02-25 21:29:16 -0800

    humanize subscription period

--- saas/templatetags/currency.py
+++ saas/templatetags/currency.py
@@ -2,9 +2,10 @@ import locale
 
 from django import template
 
-register = template.Library()
+from saas.models import Plan
 
-# XXX locale.setlocale(locale.LC_ALL, '')
+
+register = template.Library()
 
 @register.filter()
 def usd(value):
@@ -12,3 +13,19 @@ def usd(value):
         return '$0.00'
     return '$%.2f' % (float(value) / 100)
     # XXX return locale.currency(value, grouping=True)
+
+@register.filter()
+def humanize_period(period):
+    if period == Plan.INTERVAL_CHOICES[1][0]:
+        return "per hour"
+    elif period == Plan.INTERVAL_CHOICES[2][0]:
+        return "per day"
+    elif period == Plan.INTERVAL_CHOICES[3][0]:
+        return "per week"
+    elif period == Plan.INTERVAL_CHOICES[4][0]:
+        return "per month"
+    elif period == Plan.INTERVAL_CHOICES[5][0]:
+        return "per quarter"
+    elif period == Plan.INTERVAL_CHOICES[6][0]:
+        return "per year"
+    return "per ?"
