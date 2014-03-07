7e8d4c09f8ca6399dc8a4c5e85cc630076c411b7
Sebastien Mirolo <smirolo@fortylines.com>
2014-03-07 15:35:10 -0800

    fix: rename of name field to slug

--- saas/templatetags/metrics.py
+++ saas/templatetags/metrics.py
@@ -67,6 +67,6 @@ def products(organization):
         # We don't use QuerySet.distinct('organization') because the SQLite
         # backend does not support DISTINCT ON queries.
         return organization.subscriptions.all().values(
-            'organization__name').distinct()
+            'organization__slug').distinct()
     return []
 
