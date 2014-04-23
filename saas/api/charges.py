5abd2abfa14f738466833d057b172255e0030054
Sebastien Mirolo <smirolo@fortylines.com>
2014-04-22 20:28:41 -0700

    clean pylint reports

--- saas/api/charges.py
+++ saas/api/charges.py
@@ -53,8 +53,8 @@ class EmailChargeReceiptAPIView(ChargeMixin, GenericAPIView):
     """
     Email the charge receipt to the request user.
     """
-
-    def post(self, request, *args, **kwargs): #pylint: disable=unused-variable
+    #pylint: disable=unused-variable
+    def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         signals.charge_updated.send(
             sender=__name__, charge=self.object, user=request.user)
