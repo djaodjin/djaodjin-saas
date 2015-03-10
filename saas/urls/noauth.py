aee6d0c11eb505cb619cbfd16a26e2e1365118dd
Sebastien Mirolo <smirolo@djaodjin.com>
2015-03-10 16:52:42 -0700

    use slug regex in legal urls

--- saas/urls/noauth.py
+++ saas/urls/noauth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014, DjaoDjin inc.
+# Copyright (c) 2015, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -24,14 +24,15 @@
 
 from django.conf.urls import patterns, url
 
+from saas.settings import ACCT_REGEX
 from saas.views.plans import CartPlanListView
 from saas.views.legal import (sign_agreement, AgreementDetailView,
     AgreementListView)
 
 urlpatterns = patterns('',
-    url(r'^legal/(?P<slug>[\_\d\w]+)/sign$',
+    url(r'^legal/(?P<slug>%s)/sign$' % ACCT_REGEX,
         sign_agreement, name='legal_sign_agreement'),
-    url(r'^legal/(?P<slug>[\_\d\w]+)/$',
+    url(r'^legal/(?P<slug>%s)/$' % ACCT_REGEX,
         AgreementDetailView.as_view(), name='legal_agreement'),
     url(r'^legal/$', AgreementListView.as_view(), name='legal_agreement_list'),
     url(r'^pricing/', CartPlanListView.as_view(), name='saas_cart_plan_list'),
