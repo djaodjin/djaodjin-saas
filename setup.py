fcb2e273a7896e7b2c25147c67ac96753336046c
Sebastien Mirolo <smirolo@djaodjin.com>
2016-01-22 22:49:30 -0800

    install stripe_processor backend

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     install_requires=requirements,
     packages=['saas',
               'saas.backends',
+              'saas.backends.stripe_processor',
               'saas.management', # otherwise __init__.py is not installed
               'saas.management.commands',
               'saas.templatetags',
