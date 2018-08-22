aaa19f912c789dcc142cb01853b32f204fdd56f7
Dima Knivets <knivets@gmail.com>
2018-08-22 23:36:48 +0300

    resending invites command

---
+++ saas/management/commands/send_invites.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2016, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Command for sending invites"""
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+from ...utils import get_role_model
+from ... import signals
+
+class Command(BaseCommand):
+    """Send invites"""
+    help = 'Send invites'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--email', action='append', dest='email',
+            help='Send invite for this email only'
+        )
+
+    def handle(self, *args, **options):
+        emails = options.get('email')
+        roles = get_role_model().objects.filter(
+            grant_key__isnull=False)
+        if emails:
+            users = get_user_model().objects.filter(
+                email__in=emails)
+            roles = roles.filter(user__in=users)
+
+        self.stdout.write("sending invites, total: %d" % len(roles))
+
+        for role in roles:
+            signals.user_relation_added.send(sender=__name__,
+                role=role)
