djaodjin/djaodjin-saas

commit c24555b096378b6ab08a045a2422ad418dfca462

updates to generate API doc correctly

saas/api/organizations.py
@@ -40,7 +40,7 @@ from .serializers import (OrganizationCreateSerializer,
 from .. import settings, signals
 from ..compat import urlparse, urlunparse
 from ..decorators import _valid_manager
-from ..docs import swagger_auto_schema
+from ..docs import OpenAPIResponse, swagger_auto_schema
 from ..mixins import (OrganizationMixin, OrganizationSmartListMixin,
     ProviderMixin, OrganizationDecorateMixin)
 from ..models import get_broker
@@ -97,8 +97,7 @@ class OrganizationCreateMixin(object):
                             email=organization.email,
                             first_name=first_name,
                             last_name=last_name)
-                    organization.add_manager(
-                        user, request_user=self.request.user)
+                    organization.add_manager(user)
                 else:
                     # When `slug` is not present, `save` would try to create
                     # one from the `full_name`.
@@ -119,7 +118,11 @@ class OrganizationDetailAPIView(OrganizationMixin, OrganizationQuerysetMixin,
     """
     Retrieves a billing profile
 
-    **Tags**: profile
+    The API is typically used within an HTML
+    `contact information page </docs/themes/#dashboard_profile>`_
+    as present in the default theme.
+
+    **Tags**: profile, subscriber, profilemodel
 
     **Examples**
 
@@ -167,7 +170,11 @@ class OrganizationDetailAPIView(OrganizationMixin, OrganizationQuerysetMixin,
         """
         Updates a billing profile
 
-        **Tags**: profile
+        The API is typically used within an HTML
+        `contact information page </docs/themes/#dashboard_profile>`_
+        as present in the default theme.
+
+        **Tags**: profile, subscriber, profilemodel
 
         **Examples**
 
@@ -208,11 +215,15 @@ class OrganizationDetailAPIView(OrganizationMixin, OrganizationQuerysetMixin,
         """
         Deletes a billing profile
 
-        We anonymize the organization instead of purely deleting
+        We anonymize the profile instead of purely deleting
         it from the database because we don't want to loose history
         on subscriptions and transactions.
 
-        **Tags**: profile
+        The API is typically used within an HTML
+        `contact information page </docs/themes/#dashboard_profile>`_
+        as present in the default theme.
+
+        **Tags**: profile, subscriber, profilemodel
 
         **Examples**
 
@@ -336,20 +347,15 @@ class OrganizationListAPIView(OrganizationSmartListMixin,
                               OrganizationQuerysetMixin,
                               OrganizationCreateMixin, ListCreateAPIView):
     """
-    List billing profiles
+    Lists billing profiles
 
-    Queries a page (``PAGE_SIZE`` records) of organization and user profiles.
+    Returns a list of {{PAGE_SIZE}} profile and user accounts.
 
-    The queryset can be filtered for at least one field to match a search
-    term (``q``).
+    The queryset can be further refined to match a search filter (``q``)
+    and/or a range of dates ([``start_at``, ``ends_at``]),
+    and sorted on specific fields (``o``).
 
-    The queryset can be ordered by a field by adding an HTTP query parameter
-    ``o=`` followed by the field name. A sequence of fields can be used
-    to create a complete ordering by adding a sequence of ``o`` HTTP query
-    parameters. To reverse the natural order of a field, prefix the field
-    name by a minus (-) sign.
-
-    **Tags**: profile
+    **Tags**: profile, broker, profilemodel
 
     **Examples**
 
@@ -377,7 +383,13 @@ class OrganizationListAPIView(OrganizationSmartListMixin,
     serializer_class = OrganizationDetailSerializer
     user_model = get_user_model()
 
-    @swagger_auto_schema(request_body=OrganizationCreateSerializer)
+    def get_serializer_class(self):
+        if self.request.method.lower() == 'post':
+            return OrganizationCreateSerializer
+        return super(OrganizationListAPIView, self).get_serializer_class()
+
+    @swagger_auto_schema(responses={
+      201: OpenAPIResponse("Create successful", OrganizationDetailSerializer)})
     def post(self, request, *args, **kwargs):
         """
         Creates an organization, personal or user profile.
@@ -460,10 +472,14 @@ class SubscribersAPIView(OrganizationSmartListMixin,
     """
     Lists subscribers for a provider
 
-    Returns a PAGE_SIZE list of subscriber profiles which have or had
-    a subscription to a plan provided by {organization}.
+    Returns a list of {{PAGE_SIZE}} subscriber profiles which have or
+    had a subscription to a plan provided by {organization}.
 
-    **Tags**: subscriptions
+    The queryset can be further refined to match a search filter (``q``)
+    and/or a range of dates ([``start_at``, ``ends_at``]),
+    and sorted on specific fields (``o``).
+
+    **Tags**: subscriptions, provider, profilemodel
 
     **Examples**
 

saas/api/plans.py
@@ -26,24 +26,35 @@
 
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import (ListAPIView, ListCreateAPIView,
     RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
 
-from .serializers import PlanSerializer
+from .. import settings
+from ..docs import OpenAPIResponse, swagger_auto_schema
 from ..mixins import PlanMixin, CartMixin
 from ..filters import DateRangeFilter, OrderingFilter
 from ..models import Coupon, Plan, Subscription
-from .. import settings
+from .serializers import PlanSerializer, PlanCreateSerializer
 
 
 class PricingAPIView(PlanMixin, CartMixin, ListAPIView):
     """
     Lists active plans
 
-    Returns a PAGE_SIZE list of plans.
+    Returns a list of {{PAGE_SIZE}} plans which are active and can
+    be subscribed to.
 
-    **Tags**: subscriptions
+    The queryset can be further refined to match a search filter (``q``)
+    and/or a range of dates ([``start_at``, ``ends_at``]),
+    and sorted on specific fields (``o``).
+
+    The API is typically used within an HTML
+    `pricing page </docs/themes/#workflow_pricing>`_
+    as present in the default theme.
+
+    **Tags**: subscriptions, visitor, planmodel
 
     **Examples**
 
@@ -79,13 +90,15 @@ class PricingAPIView(PlanMixin, CartMixin, ListAPIView):
           }]
         }
     """
-    serializer_class = PlanSerializer
-    filter_backends = (DateRangeFilter, OrderingFilter)
-    ordering_fields = [
+    ordering_fields = (
         ('title', 'title'),
         ('period_amount', 'period_amount'),
         ('is_active', 'is_active'),
-        ('created_at', 'created_at')]
+        ('created_at', 'created_at'),
+    )
+    ordering = ('period_amount',)
+    filter_backends = (DateRangeFilter, OrderingFilter)
+    serializer_class = PlanSerializer
 
     def get_queryset(self):
         queryset = Plan.objects.filter(organization=self.provider,
@@ -119,15 +132,19 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
     """
     Lists a provider plans
 
-    Returns a PAGE_SIZE list of plans whose provider is {organization}.
+    Returns a list of {{PAGE_SIZE}} plans managed by provider {organization}.
 
-    **Tags**: subscriptions
+    The queryset can be further refined to match a search filter (``q``)
+    and/or a range of dates ([``start_at``, ``ends_at``]),
+    and sorted on specific fields (``o``).
+
+    **Tags**: subscriptions, provider, planmodel
 
     **Examples**
 
     .. code-block:: http
 
-         GET /api/profile/cowork/plans HTTP/1.1
+         GET /api/profile/cowork/plans/ HTTP/1.1
 
     responds
 
@@ -157,32 +174,41 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
           }]
         }
     """
-    serializer_class = PlanSerializer
-    filter_backends = (DateRangeFilter, OrderingFilter)
-    ordering_fields = [
+    ordering_fields = (
         ('title', 'title'),
         ('period_amount', 'period_amount'),
         ('is_active', 'is_active'),
-        ('created_at', 'created_at')]
+        ('created_at', 'created_at')
+    )
+    ordering = ('period_amount',)
+    filter_backends = (DateRangeFilter, OrderingFilter)
+    serializer_class = PlanSerializer
 
+    def get_serializer_class(self):
+        if self.request.method.lower() == 'post':
+            return PlanCreateSerializer
+        return super(PlanListCreateAPIView, self).get_serializer_class()
+
+    @swagger_auto_schema(responses={
+      201: OpenAPIResponse("Create successful", PlanSerializer)})
     def post(self, request, *args, **kwargs):
         """
-        Creates a subscription plan
+        Creates a plan
 
-        Creates a subscription plan for provider {organization}.
+        Creates a new subscription plan that belongs to provider {organization}.
 
-        **Tags**: subscriptions
+        **Tags**: subscriptions, provider, planmodel
 
         **Examples**
 
         .. code-block:: http
 
-             POST /api/profile/cowork/plans HTTP/1.1
+             POST /api/profile/cowork/plans/ HTTP/1.1
 
         .. code-block:: json
 
             {
-                "title": "Open Space",
+                "title": "Popup Desk",
                 "description": "A desk in our coworking space",
                 "is_active": false,
                 "period_amount": 12000,
@@ -194,7 +220,7 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
         .. code-block:: json
 
             {
-                "title": "Open Space",
+                "title": "Popup Desk",
                 "description": "A desk in our coworking space",
                 "is_active": false,
                 "period_amount": 12000,
@@ -213,6 +239,10 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
         return queryset
 
     def perform_create(self, serializer):
+        if self.organization.plans.filter(
+                title=serializer.validated_data.get('title')).exists():
+            raise ValidationError(_("A plan with this title already exists."))
+
         unit = serializer.validated_data.get('unit', None)
         if not unit:
             first_plan = self.get_queryset().first()
@@ -225,7 +255,7 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
 
 class PlanDetailAPIView(PlanMixin, RetrieveUpdateDestroyAPIView):
     """
-    Retrieves a subscription plan
+    Retrieves a plan
 
     Returns the {plan} for provider {organization}
 
@@ -233,13 +263,17 @@ class PlanDetailAPIView(PlanMixin, RetrieveUpdateDestroyAPIView):
     to subscribe to it, or deactivate a plan, disabling users from subscribing
     to it.
 
-    **Tags**: subscriptions
+    The API is typically used within an HTML
+    `update plan page </docs/themes/#profile_plans_plan>`_
+    as present in the default theme.
+
+    **Tags**: subscriptions, provider, planmodel
 
     **Examples**
 
     .. code-block:: http
 
-        GET /api/profile/cowork/plans/open-space HTTP/1.1
+        GET /api/profile/cowork/plans/open-space/ HTTP/1.1
 
     responds
 
@@ -257,20 +291,24 @@ class PlanDetailAPIView(PlanMixin, RetrieveUpdateDestroyAPIView):
 
     def delete(self, request, *args, **kwargs):
         """
-        Deletes a subscription plan
+        Deletes a plan
 
         A plan can only be deleted when there are no subscriptions to it.
         Even if all subscriptions to a plan have expired, the plan cannot
         be deleted. It should be de-activated instead such that no customers
         can subscribes to it.
 
-        **Tags**: subscriptions
+        The API is typically used within an HTML
+        `update plan page </docs/themes/#profile_plans_plan>`_
+        as present in the default theme.
+
+        **Tags**: subscriptions, provider, planmodel
 
         **Examples**
 
         .. code-block:: http
 
-            DELETE /api/profile/cowork/plans/open-space HTTP/1.1
+            DELETE /api/profile/cowork/plans/open-space/ HTTP/1.1
         """
         return super(PlanDetailAPIView, self).delete(request, *args, **kwargs)
 
@@ -331,7 +369,7 @@ class PlanDetailAPIView(PlanMixin, RetrieveUpdateDestroyAPIView):
 
     def put(self, request, *args, **kwargs):
         """
-        Updates a subscription plan
+        Updates a plan
 
         Updates fields for {plan}. If the ``period_amount`` is modified,
         all subscriptions to this plan will be charged the ``period_amount``
@@ -341,13 +379,17 @@ class PlanDetailAPIView(PlanMixin, RetrieveUpdateDestroyAPIView):
         to subscribe to it, or deactivate a plan, disabling users
         from subscribing to it.
 
-        **Tags**: subscriptions
+        The API is typically used within an HTML
+        `update plan page </docs/themes/#profile_plans_plan>`_
+        as present in the default theme.
+
+        **Tags**: subscriptions, provider, planmodel
 
         **Examples**
 
         .. code-block:: http
 
-            PUT /api/profile/cowork/plans/open-space HTTP/1.1
+            PUT /api/profile/cowork/plans/open-space/ HTTP/1.1
 
         .. code-block:: json
 

saas/pagination.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, DjaoDjin inc.
+# Copyright (c) 2021, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,8 +27,9 @@ from collections import OrderedDict
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-from .models import sum_dest_amount, sum_orig_amount, sum_balance_amount
 from . import settings
+from .models import (sum_dest_amount, sum_orig_amount, sum_balance_amount,
+    Transaction)
 
 
 class BalancePagination(PageNumberPagination):
@@ -55,6 +56,7 @@ class BalancePagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
+            ('start_at', self.start_at),
             ('ends_at', self.ends_at),
             ('balance_amount', self.balance_amount),
             ('balance_unit', self.balance_unit),
@@ -64,24 +66,232 @@ class BalancePagination(PageNumberPagination):
             ('results', data)
         ]))
 
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'start_at': {
+                    'type': 'string',
+                    'format': 'date',
+                    'description': "Start of the date range for which"\
+                        " the balance was computed"
+                },
+                'ends_at': {
+                    'type': 'string',
+                    'format': 'date',
+                    'description': "End of the date range for which"\
+                        " the balance was computed"
+                },
+                'balance_amount': {
+                    'type': 'integer',
+                    'description': "balance of all transactions in cents"\
+                        " (i.e. 100ths) of unit"
+                },
+                'balance_unit': {
+                    'type': 'integer',
+                    'description': "three-letter ISO 4217 code"\
+                        " for currency unit (ex: usd)"
+                },
+                'count': {
+                    'type': 'integer',
+                    'description': "The number of records"
+                },
+                'next': {
+                    'type': 'string',
+                    'description': "API end point to get the next page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'previous': {
+                    'type': 'string',
+                    'description': "API end point to get the previous page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'results': schema,
+            },
+        }
 
-class TotalPagination(PageNumberPagination):
 
-    def paginate_queryset(self, queryset, request, view=None):
-        self.total = 0
-        for charge in queryset:
-            self.total += charge.amount
-        return super(TotalPagination, self).paginate_queryset(
-            queryset, request, view=view)
+class RoleListPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
-            ('total', self.total),
+            ('invited_count', self.request.invited_count),
+            ('requested_count', self.request.requested_count),
             ('count', self.page.paginator.count),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('results', data)
         ]))
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'invited_count': {
+                    'type': 'integer',
+                    'description': "Number of user invited to have a role"
+                },
+                'requested_count': {
+                    'type': 'integer',
+                    'description': "Number of user requesting a role"
+                },
+                'count': {
+                    'type': 'integer',
+                    'description': "The number of records"
+                },
+                'next': {
+                    'type': 'string',
+                    'description': "API end point to get the next page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'previous': {
+                    'type': 'string',
+                    'description': "API end point to get the previous page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'results': schema,
+            },
+        }
+
+
+class StatementBalancePagination(PageNumberPagination):
+    """
+    Decorate the results of an API call with the balance as shown
+    in an organization statement.
+    """
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.start_at = view.start_at
+        self.ends_at = view.ends_at
+        self.balance_amount, self.balance_unit \
+            = Transaction.objects.get_statement_balance(view.organization)
+        return super(StatementBalancePagination, self).paginate_queryset(
+            queryset, request, view=view)
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('start_at', self.start_at),
+            ('ends_at', self.ends_at),
+            ('balance_amount', self.balance_amount),
+            ('balance_unit', self.balance_unit),
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'start_at': {
+                    'type': 'string',
+                    'format': 'date',
+                    'description': "Start of the date range for which"\
+                        " the balance was computed"
+                },
+                'ends_at': {
+                    'type': 'string',
+                    'format': 'date',
+                    'description': "End of the date range for which"\
+                        " the balance was computed"
+                },
+                'balance_amount': {
+                    'type': 'integer',
+                    'description': "balance of all transactions in cents"\
+                        " (i.e. 100ths) of unit"
+                },
+                'balance_unit': {
+                    'type': 'integer',
+                    'description': "three-letter ISO 4217 code"\
+                        " for currency unit (ex: usd)"
+                },
+                'count': {
+                    'type': 'integer',
+                    'description': "The number of records"
+                },
+                'next': {
+                    'type': 'string',
+                    'description': "API end point to get the next page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'previous': {
+                    'type': 'string',
+                    'description': "API end point to get the previous page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'results': schema,
+            },
+        }
+
+
+class TotalPagination(PageNumberPagination):
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.start_at = view.start_at
+        self.ends_at = view.ends_at
+        self.totals = view.totals
+        return super(TotalPagination, self).paginate_queryset(
+            queryset, request, view=view)
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('start_at', self.start_at),
+            ('ends_at', self.ends_at),
+            ('balance_amount', self.totals['amount']),
+            ('balance_unit', self.totals['unit']),
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'balance_amount': {
+                    'type': 'integer',
+                    'description': "The sum of all record amounts (in unit)"
+                },
+                'balance_unit': {
+                    'type': 'integer',
+                    'description': "three-letter ISO 4217 code"\
+                        " for currency unit (ex: usd)"
+                },
+                'count': {
+                    'type': 'integer',
+                    'description': "The number of records"
+                },
+                'next': {
+                    'type': 'string',
+                    'description': "API end point to get the next page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'previous': {
+                    'type': 'string',
+                    'description': "API end point to get the previous page"\
+                        "of records matching the query",
+                    'nullable': True,
+                    'format': 'uri',
+                },
+                'results': schema,
+            },
+        }
 
 
 class TypeaheadPagination(PageNumberPagination):
@@ -102,3 +312,18 @@ class TypeaheadPagination(PageNumberPagination):
             ('count', self.count),
             ('results', data)
         ]))
+
+    def get_schema_operation_parameters(self, view):
+        return []
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            'type': 'object',
+            'properties': {
+                'count': {
+                    'type': 'integer',
+                    'description': "The number of records"
+                },
+                'results': schema,
+            },
+        }

saas/settings.py
@@ -120,6 +120,8 @@ _SETTINGS = {
     },
     'PROCESSOR_BACKEND_CALLABLE': None,
     'ROLE_RELATION': 'saas.Role',
+    'ROLE_SERIALIZER': 'saas.api.serializers.RoleSerializer',
+    'USER_SERIALIZER': 'saas.api.serializers.UserSerializer',
     'SEARCH_FIELDS_PARAM': 'q_f',
     'TERMS_OF_USE': 'terms-of-use',
 }
@@ -157,6 +159,8 @@ PROCESSOR_ID = PROCESSOR.get('INSTANCE_PK', 1)
 PROCESSOR_HOOK_URL = PROCESSOR.get('WEBHOOK_URL', 'stripe/postevent')
 PROCESSOR_HOOK_SECRET = PROCESSOR.get('WEBHOOK_SECRET')
 ROLE_RELATION = _SETTINGS.get('ROLE_RELATION')
+ROLE_SERIALIZER = _SETTINGS.get('ROLE_SERIALIZER')
+USER_SERIALIZER = _SETTINGS.get('USER_SERIALIZER')
 SEARCH_FIELDS_PARAM = _SETTINGS.get('SEARCH_FIELDS_PARAM')
 TERMS_OF_USE = _SETTINGS.get('TERMS_OF_USE')
 

commit 3e23b5337ba7b3b4ebc990bc6012fdb9ef262357

fixes issues introduced by commit c24555b0

saas/api/organizations.py
@@ -448,7 +448,8 @@ class OrganizationListAPIView(OrganizationSmartListMixin,
         self.decorate_personal(organization)
 
         # returns created profile
-        serializer = self.get_serializer(instance=organization)
+        serializer = self.serializer_class(instance=organization,
+            context=self.get_serializer_context())
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data,
             status=status.HTTP_201_CREATED, headers=headers)

saas/api/plans.py
@@ -241,7 +241,8 @@ class PlanListCreateAPIView(PlanMixin, ListCreateAPIView):
     def perform_create(self, serializer):
         if self.organization.plans.filter(
                 title=serializer.validated_data.get('title')).exists():
-            raise ValidationError(_("A plan with this title already exists."))
+            raise ValidationError({
+                'title': _("A plan with this title already exists.")})
 
         unit = serializer.validated_data.get('unit', None)
         if not unit:

saas/api/roles.py
@@ -261,7 +261,8 @@ class OptinBase(OrganizationDecorateMixin, OrganizationCreateMixin):
         # XXX There is currently a single relation created due to statement
         # `organizations = [organization]` earlier in the code.
         if notified:
-            resp_serializer = self.get_serializer(notified[0])
+            resp_serializer = self.serializer_class(
+                instance=notified[0], context=self.get_serializer_context())
             result = resp_serializer.data
         else:
             result = None

saas/pagination.py
@@ -39,6 +39,7 @@ class BalancePagination(PageNumberPagination):
     """
 
     def paginate_queryset(self, queryset, request, view=None):
+        self.start_at = view.start_at
         self.ends_at = view.ends_at
         if view.selector is not None:
             dest_totals = sum_dest_amount(queryset.filter(

testsite/settings.py
@@ -112,7 +112,7 @@ REST_FRAMEWORK = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'testsite', 'media')
 MEDIA_URL = '/media/'
 
-APP_STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
+APP_STATIC_ROOT = os.path.join(BASE_DIR, 'testsite', 'static')
 STATIC_URL = '/static/'
 
 if DEBUG: