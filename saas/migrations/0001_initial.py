efbcf57f86b8b40fbc050a9563988761e7a64daf
Amir Raouf <amir2raouf@gmail.com>
2022-07-25 17:31:51 +0200

    Fix migrations

--- saas/migrations/0001_initial.py
+++ saas/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        migrations.swappable_dependency(settings.SAAS_ORGANIZATION_MODEL),
     ]
 
     operations = [
