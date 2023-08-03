djaodjin/djaodjin-saas

commit 5571ad8884433e253e37b64971738d0e0b52f79e

uses <template> for messages if defined in DOM

saas/static/js/djaodjin-resources-vue.js
@@ -106,23 +106,39 @@ var messagesMixin = {
                     toastr[style](messages[i]);
                 }
             } else {
-                var messageBlock = "<div class=\"alert alert-block alert-dismissible fade show";
+                var blockStyle = "";
                 if( style ) {
                     if( style === "error" ) {
                         style = "danger";
                     }
-                    messageBlock += " alert-" + style;
+                    blockStyle = " alert-" + style;
                 }
-                messageBlock += "\">";
+                var messageBlock = messagesElement.find(
+                    ".alert" + blockStyle.replace(' ', '.'));
+                if( messageBlock.length === 0 ) {
+                    const blockText = "<div class=\"alert" + blockStyle
+                        + " alert-dismissible fade show\">"
+                        + "<button type=\"button\" class=\"btn-close\""
+                        + " data-bs-dismiss=\"alert\" aria-label=\"Close\">"
+                        + "</button></div>";
+                    var div = document.createElement('div');
+                    div.innerHTML = blockText;
+                    messageBlock = jQuery(div.firstChild);
+                } else {
+                    messageBlock = jQuery(messageBlock[0].cloneNode(true));
+                }
 
+                // insert the actual messages
                 if( typeof messages === "string" ) {
                     messages = [messages];
                 }
                 for( var i = 0; i < messages.length; ++i ) {
-                    messageBlock += "<div>" + messages[i] + "</div>";
+                    messageBlock.append("<div>" + messages[i] + "</div>");
                 }
-                messageBlock += "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button></div>";
-                vm.getMessagesElement().append(messageBlock);
+                if( messageBlock.css('display') === 'none' ) {
+                    messageBlock.css('display', 'block');
+                }
+                messagesElement.append(messageBlock);
             }
             var messagesContainer = messagesElement.parent();
             if( messagesContainer && messagesContainer.hasClass("hidden") ) {

saas/static/js/djaodjin-resources.js
@@ -32,23 +32,40 @@ function showMessages(messages, style) {
         }
 
     } else {
-        var messageBlock = "<div class=\"alert alert-block alert-dismissible fade show";
+        var messagesElement = jQuery("#messages-content");
+        var blockStyle = "";
         if( style ) {
             if( style === "error" ) {
                 style = "danger";
             }
-            messageBlock += " alert-" + style;
+            blockStyle = " alert-" + style;
         }
-        messageBlock += "\">";
+        var messageBlock = messagesElement.find(
+            ".alert" + blockStyle.replace(' ', '.'));
+        if( messageBlock.length === 0 ) {
+            const blockText = "<div class=\"alert" + blockStyle
+                  + " alert-dismissible fade show\">"
+                  + "<button type=\"button\" class=\"btn-close\""
+                  + " data-bs-dismiss=\"alert\" aria-label=\"Close\">"
+                  + "</button></div>";
+            var div = document.createElement('div');
+            div.innerHTML = blockText;
+            messageBlock = jQuery(div.firstChild);
+        } else {
+            messageBlock = jQuery(messageBlock[0].cloneNode(true));
+        }
 
+        // insert the actual messages
         if( typeof messages === "string" ) {
             messages = [messages];
         }
         for( var i = 0; i < messages.length; ++i ) {
-            messageBlock += "<div>" + messages[i] + "</div>";
-         }
-         messageBlock += "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button></div>";
-         jQuery("#messages-content").append(messageBlock);
+            messageBlock.append("<div>" + messages[i] + "</div>");
+        }
+        if( messageBlock.css('display') === 'none' ) {
+            messageBlock.css('display', 'block');
+        }
+        messagesElement.append(messageBlock);
     }
     jQuery("#messages").removeClass("hidden");
     jQuery("html, body").animate({

commit 2bdae414bf29dfc94a307e92206580d162efca30

fixes regression introduced in previous commit (5571ad88)

saas/static/js/djaodjin-resources-vue.js
@@ -126,6 +126,7 @@ var messagesMixin = {
                     messageBlock = jQuery(div.firstChild);
                 } else {
                     messageBlock = jQuery(messageBlock[0].cloneNode(true));
+                    messageBlock.find('div').remove();
                 }
 
                 // insert the actual messages

saas/static/js/djaodjin-resources.js
@@ -53,6 +53,7 @@ function showMessages(messages, style) {
             messageBlock = jQuery(div.firstChild);
         } else {
             messageBlock = jQuery(messageBlock[0].cloneNode(true));
+            messageBlock.find('div').remove();
         }
 
         // insert the actual messages

saas/templates/saas/profile/plans/plan.html
@@ -28,7 +28,7 @@
         {{form.advance_discount_type}}
         {{form.advance_discount_value}}
         {{form.advance_discount_length}}
-        <button type="submit">Submit</button>
+        <button type="submit">{% if form.submit_title %}{{form.submit_title}}{% else %}Submit{% endif %}</button>
     </form>
     {% if show_delete %}
     <div id="confirm-delete" data-plan="{{form.instance.slug}}">