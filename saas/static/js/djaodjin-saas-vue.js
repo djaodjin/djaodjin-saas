djaodjin/djaodjin-saas

commit 08e74c4931a5e749d23fa4973f110a73795f0c03

ticket 155

saas/static/js/djaodjin-saas-vue.js
@@ -3136,7 +3136,26 @@ Vue.component('search-profile', {
     data: function() {
         return {
             url: this.$urls.provider ? this.$urls.provider.api_accounts : null,
+            listUrl: this.$urls.organization ?
+                this.$urls.organization.api_profile_base : null,
         }
+    },
+    methods: {
+        search: function() {
+            var vm = this;
+            if( !vm.query ) return vm.reset();
+            var params = {};
+            params[vm.queryParamName] = vm.query;
+
+            vm.loading = true;
+            vm.reqGet(vm.listUrl, params,
+            function(resp) {
+                vm.items = resp.results;
+                vm.loading = false;
+            }, function() {
+                vm.loading = false;
+            });
+        },
     }
 });
 

commit c85e5f41a790e53a83b0bc75b5fc02bb81ec7471

fixes regression introduced by commit 08e74c49
Overriding `search()` prevent the typeahead component in _profile_new.html
to show the profiles a user might have implicit role on.

saas/static/js/djaodjin-saas-vue.js
@@ -3128,8 +3128,22 @@ Vue.component('unengaged-subscribers', {
 
 // Widgets for dashboard
 // ---------------------
+Vue.component('profile-typeahead', {
+    mixins: [
+        typeAheadMixin
+    ],
+    data: function() {
+        return {
+            url: this.$urls.provider ? this.$urls.provider.api_accounts : null,
+        }
+    },
+});
+
 
 Vue.component('search-profile', {
+    // The difference of 'search-profile' with 'profile-typeahead' is that
+    // calling `search` on 'search-profile' will make an API call to the
+    // profile list API instead of the profile typeahead API.
     mixins: [
         typeAheadMixin
     ],
@@ -3158,6 +3172,8 @@ Vue.component('search-profile', {
         },
     }
 });
+
+
 
 
 Vue.component('subscription-typeahead', {

saas/templates/saas/_profile_new.html
@@ -5,7 +5,7 @@ Request access to an existing profile or create a new profile
   <div>
     <form id="add-profile" @submit.prevent="submit">
       <input type="hidden" name="csrfmiddlewaretoken" value="{{csrf_token}}">
-      <search-profile inline-template ref="typeahead"
+      <profile-typeahead inline-template ref="typeahead"
                  v-bind:dataset="items.results"
                  v-on:selectitem="updateItemSelected"
                  v-show="!inNewProfileFlow"
@@ -47,7 +47,7 @@ We couldn't find a profile matching your search criteria, or there are too many 
             </div>
           </div>
         </div>
-      </search-profile>
+      </profile-typeahead>
       <!-- Workflow to create a new profile  -->
       <div v-show="inNewProfileFlow">
         <div>